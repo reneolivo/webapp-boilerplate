11a774e316dc1e6f69bd997e7a002c8a36c78911
René Olivo <minet.ws@gmail.com>
2017-02-26 10:53:41 -0400

    separate articles from app

--- src/app.js
+++ src/app.js
@@ -1,29 +1,14 @@
 import './styles/style.scss';
 import jQuery from 'jquery';
 import template from './app.pug';
+import articles from './articles';
 
 export default class App {
   name = 'Sample App';
 
-  static articles = [
-    {
-      title: 'A Jade tutorial for begginers',
-      link: 'https://www.sitepoint.com/jade-tutorial-for-beginners/'
-    },
-    {
-      title: 'ES6 complete tutorial',
-      link: 'http://qnimate.com/post-series/ecmascript-6-complete-tutorial/'
-    },
-    {
-      title: 'Getting Real Book',
-      link: 'https://gettingreal.37signals.com/'
-    }
-  ];
-
-
   static init() {
     const body = jQuery('body');
-    const html = template({ articles: this.articles });
+    const html = template({ articles: articles });
 
     body.append(html);
   }
---
+++ src/articles.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: 'A Jade tutorial for begginers',
+    link: 'https://www.sitepoint.com/jade-tutorial-for-beginners/'
+  },
+  {
+    title: 'ES6 complete tutorial',
+    link: 'http://qnimate.com/post-series/ecmascript-6-complete-tutorial/'
+  },
+  {
+    title: 'Getting Real Book',
+    link: 'https://gettingreal.37signals.com/'
+  }
+];
